3793a3b5ac19baa947f0ecb2a937bf8f88a2e185
Felix Dulfer <felix@dulfer.me>
2025-07-07 19:59:54 +0200

    feat(app): add pre-filled examples for various period types in the form

--- projects/demo-app/src/app/app.component.ts
+++ projects/demo-app/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { PeriodPickerComponent } from '../../../ng-mat-period-picker/src/lib/components/period-picker.component';
 import { YearMonthFieldComponent } from '../../../ng-mat-period-picker/src/lib/components/year-month-field.component';
+import { YearMonth, Period } from '../../../ng-mat-period-picker/src/lib/types';
 
 @Component({
   selector: 'app-root',
@@ -51,6 +52,44 @@ import { YearMonthFieldComponent } from '../../../ng-mat-period-picker/src/lib/c
         presentPlaceholder="Still studying"
       />
       <pre>{{ form.value.educationPeriod | json }}</pre>
+
+      <h2>Pre-filled Examples</h2>
+
+      <h3>Complete Period (Start + End)</h3>
+      <lib-ng-mat-period-picker
+        formControlName="completePeriod"
+        startLabel="Start Date"
+        endLabel="End Date"
+        presentLabel="Present"
+      />
+      <pre>{{ form.value.completePeriod | json }}</pre>
+
+      <h3>Current Period (Start + Present)</h3>
+      <lib-ng-mat-period-picker
+        formControlName="currentPeriod"
+        startLabel="Start Date"
+        endLabel="End Date"
+        presentLabel="Present"
+      />
+      <pre>{{ form.value.currentPeriod | json }}</pre>
+
+      <h3>Year Only Period (Start Year + End Year)</h3>
+      <lib-ng-mat-period-picker
+        formControlName="yearOnlyPeriod"
+        startLabel="Start Year"
+        endLabel="End Year"
+        presentLabel="Present"
+      />
+      <pre>{{ form.value.yearOnlyPeriod | json }}</pre>
+
+      <h3>Mixed Period (Start Year/Month + End Year Only)</h3>
+      <lib-ng-mat-period-picker
+        formControlName="mixedPeriod"
+        startLabel="Start Date"
+        endLabel="End Year"
+        presentLabel="Present"
+      />
+      <pre>{{ form.value.mixedPeriod | json }}</pre>
     </form>
   `,
 })
@@ -61,5 +100,25 @@ export class AppComponent {
     period: new FormControl(null),
     customPeriod: new FormControl(null),
     educationPeriod: new FormControl(null),
+    completePeriod: new FormControl<Period>({
+      start: { year: 2020, month: 3 },
+      end: { year: 2023, month: 12 },
+      isPresent: false,
+    }),
+    currentPeriod: new FormControl<Period>({
+      start: { year: 2022, month: 6 },
+      end: null,
+      isPresent: true,
+    }),
+    yearOnlyPeriod: new FormControl<Period>({
+      start: { year: 2018, month: null },
+      end: { year: 2022, month: null },
+      isPresent: false,
+    }),
+    mixedPeriod: new FormControl<Period>({
+      start: { year: 2021, month: 9 },
+      end: { year: 2024, month: null },
+      isPresent: false,
+    }),
   });
 }
